bb658a55ed295c82aec64a0c554fbd7e15e9df63
ydshieh <ydshieh@users.noreply.github.com>
2025-06-01 11:12:49 +0200

    fix: return_dict=True

--- src/transformers/models/qwen2_vl/modeling_qwen2_vl.py
+++ src/transformers/models/qwen2_vl/modeling_qwen2_vl.py
@@ -1469,7 +1469,7 @@ class Qwen2VLForConditionalGeneration(Qwen2VLPreTrainedModel, GenerationMixin):
         output_hidden_states = (
             output_hidden_states if output_hidden_states is not None else self.config.output_hidden_states
         )
-        return_dict = return_dict if return_dict is not None else self.config.use_return_dict
+        return_dict = True
 
         outputs = self.model(
             input_ids=input_ids,
